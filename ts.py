6f36ab300c02a787a65ea1a2a3203f0ae659cf1b
AaronLopes <aaronlopes@me.com>
2017-07-15 11:20:26 -0700

    fixed sentiment

--- ts.py
+++ ts.py
@@ -11,8 +11,6 @@ import matplotlib.pyplot as plt
 import requests
 import json
 
-
-
 consumer_key = 'oZNDqncnvc9hFDwYocRGukS0V'
 consumer_key_secret = 'wh4kSf3Xa11HWdShuW3Wdqkgdq67wOAeW7FCeTklUltshF6eZA'
 
@@ -24,16 +22,19 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 topic_dict = json.loads(open("topic_data.json", "r").read())
-sent_dict = {}
 
 handles = open("influencers.txt", "r")
-
+np.sent_scores = []
 
 def general_scrape(query):
 	q = query
 	tweet_struct = {}
 	tweet_struct[q] = {}
 	tweets = api.search(q=query)
+	sum_pos = 0
+	sum_neg = 0
+	sum_neu = 0
+	tweet_counter = 0
 	for tweet in tweets:
 		id = str(tweet.id)
 		if id not in tweet_struct[q]:
@@ -41,15 +42,35 @@ def general_scrape(query):
 		tweet_struct[q][id]['favorites'] = tweet.favorite_count
 		tweet_struct[q][id]['retweets'] = tweet.retweet_count
 		tweet_struct[q][id]['text'] = tweet.text.encode("utf-8")
+		t = tweet.text.encode("utf-8")
+		sent_data = [
+			('text', t)
+		]
+		sent_result = requests.post('http://text-processing.com/api/sentiment/', data=sent_data)
+		sent_json = sent_result.json()
+		sent_dict = {}
+		sent_dict['pos'] = int(round(sent_json['probability']['pos'] * 100))
+		sum_pos += sent_dict['pos']
+		sent_dict['neg'] = int(round(sent_json['probability']['neg'] * 100))
+		sum_neg += sent_dict['neg']
+		sent_dict['neutral'] = int(round(sent_json['probability']['neutral'] * 100))
+		sum_neu += sent_dict['neutral']
+		tweet_struct[q][id]['sentiment'] = sent_dict
+		tweet_counter += 1
+	sent_scores.append((sum_pos / tweet_counter, sum_neg / tweet_counter, sum_neu / tweet_counter))
 	return tweet_struct
 
 
 def scrape_user(user_list):
 	user_struct = {}
 	user_tweets = []
+	sum_pos = 0
+	sum_neg = 0
+	sum_neu = 0
+	tweet_counter = 0
 	for user in user_list:
 		try:
-			user_tweets = api.user_timeline(id=user, count=20)
+			user_tweets = api.user_timeline(id=user, count=50)
 			print("[SCRAPING] " + str(user) + "...")
 		except tweepy.TweepError as e:
 			print("*** Encountered exception: " + str(e))
@@ -63,38 +84,37 @@ def scrape_user(user_list):
 			user_struct[u][id]['favorites'] = tweet.favorite_count
 			user_struct[u][id]['retweets'] = tweet.retweet_count
 			user_struct[u][id]['followers'] = tweet.user.followers_count
-			user_struct[u][id]['sentiment'] = calc_sent(tweet)
-
+			t = tweet.text.encode("utf-8")
+			sent_data = [
+				('text', t)
+			]
+			sent_result = requests.post('http://text-processing.com/api/sentiment/', data=sent_data)
+			sent_json = sent_result.json()
+			sent_dict = {}
+			sent_dict['pos'] = int(round(sent_json['probability']['pos'] * 100))
+			sum_pos += sent_dict['pos']
+			sent_dict['neg'] = int(round(sent_json['probability']['neg'] * 100))
+			sum_neg += sent_dict['neg']
+			sent_dict['neutral'] = int(round(sent_json['probability']['neutral'] * 100))
+			sum_neu += sent_dict['neutral']
+			user_struct[u][id]['sentiment'] = sent_dict
+			tweet_counter +=1
+	sent_scores.append((sum_pos/tweet_counter, sum_neg/tweet_counter, sum_neu/tweet_counter))
 	return user_struct
 
-
-def calc_sent(tweet):
-	tweet_text = tweet.text.encode("utf-8")
-	sent_data = [
-		('text', tweet_text)
-	]
-	sent_result = requests.post('http://text-processing.com/api/sentiment/', data=sent_data)
-	sent_json = sent_result.json()
-	sent_dict['pos'] = int(round(sent_json['probability']['pos'] * 100))
-	sent_dict['neg'] = int(round(sent_json['probability']['neg'] * 100))
-	sent_dict['neutral'] = int(round(sent_json['probability']['neutral'] * 100))
-	return sent_dict
 
 def struct_json(topic):
 	t = topic
 	topic_dict[t] = {}
 	topic_dict[t]['general'] = general_scrape(topic)
-	topic_dict[t]['users'] = scrape_user(open('influencers.txt', 'r'))
+	#topic_dict[t]['users'] = scrape_user(open('influencers.txt', 'r'))
+	print(sent_scores)
 	return topic_dict
 
-def plot():
-	mu_pos = np.mean(sent_dict['pos'])
-	mu_neg = np.mean(sent_dict['neg'])
-	mu_neu = np.mean(sent_dict['neutral'])
+def plot(score_array):
 	labels = ('Positive', 'Negative', 'Neutral')
 	y_pos = np.arange(len(labels))
 	performance = [mu_pos, mu_neg, mu_neu]
-
 	plt.barh(y_pos, performance, align='center', alpha=0.6)
 	plt.yticks(y_pos, labels)
 	plt.xlabel('Sentiment Score')
